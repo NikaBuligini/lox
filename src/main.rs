140a90f86c67174439ac5fa7391baa9e69ff9b35
Nikoloz Buligini <nbu@templafy.com>
2024-09-08 14:09:03 +0200

    Keep going after error

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,7 @@ enum Commands {
 
 fn main() -> miette::Result<()> {
     let args = Args::parse();
+    let mut any_cc_err = false;
     match args.command {
         Commands::Tokenize { filename } => {
             let file_contents = fs::read_to_string(&filename)
@@ -25,11 +26,30 @@ fn main() -> miette::Result<()> {
                 .wrap_err_with(|| format!("reading '{}' failed", filename.display()))?;
 
             for token in Lexer::new(&file_contents) {
-                let token = token?;
+                let token = match token {
+                    Ok(t) => t,
+                    Err(e) => {
+                        eprintln!("{e:?}");
+                        if let Some(unrecognized) = e.downcast_ref::<SingleTokenError>() {
+                            any_cc_err = true;
+                            eprintln!(
+                                "[line {}] Error: Unexpected character: {}",
+                                unrecognized.line(),
+                                unrecognized.token
+                            );
+                            std::process::exit(65);
+                        }
+                        continue;
+                    }
+                };
                 println!("{token}");
             }
             println!("EOF null");
         }
+    }
+
+    if any_cc_err {
+        std::process::exit(65);
     }
 
     Ok(())
